7ea8290e012be7c78a5742dd5e9c49fe048f9728
Ronit Shternfeld <ronits@earnix.com>
2023-07-10 20:09:38 +0300

    functional

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { ChangeEvent, useState } from "react";
 import { TopBar } from "./TopBar";
 import { GoogleMap } from "./GMaps";
 // const log = (...args) => console.log.apply(null, ["App -->", ...args]);
@@ -6,37 +6,93 @@ const log = (...args: any[]) => console.log("App -->", ...args);
 
 
 interface State {
-  lat: number;
-  lng: number;
+  center: {
+    lat: number;
+    lng: number;
+};
+  zoom: number;
 }
 
-export class App extends Component<object, State> {
-  state = {
-    lat: -34.397,
-    lng: 150.644,
-  };
+export function App() {
+  const [center, setState] = useState({lat: -34.397, lng: 150.644});
+  const [zoom, setZoom] = useState(8);
 
-  reposition(city: string) {
+  function reposition(city: string) {
     switch (city) {
       case "tel aviv":
-        this.setState({ lat: 32.0042938, lng: 34.7615399 });
+        setState({ lat: 32.0042938, lng: 34.7615399 });
+      break;
+      case "London":
+        setState({ lat: 32.0042938, lng: 34.7615399 });
         break;
+      case "Paris":
+        setState({ lat: 32.0042938, lng: 34.7615399 });
+        break;
+      
       default:
         alert("wrong city");
     }
   }
 
-  render() {
-    log(this.state);
+  const set_zoom = (event: ChangeEvent<HTMLInputElement>) => {
+    setZoom(Number(event.target.value));
+  };
+
     return (
       <div className="app">
         <TopBar><h1>Google Maps Example in React</h1></TopBar>
         <div className="hbox mb20">
-          <button onClick={() => this.reposition("tel aviv")}>Tel Aviv</button>
-          <input type="number" min="8" max="16" placeholder="8" />
+          <button onClick={() => reposition("tel aviv")}>Tel Aviv</button>
+          <button onClick={() => reposition("London")}>London</button>
+          <button onClick={() => reposition("Paris")}>Paris</button>
+          <input type="number" min="8" max="16" placeholder="8" onChange={set_zoom}/>
         </div>
-        <GoogleMap lat={this.state.lat} lng={this.state.lng} />
+        <GoogleMap lat={center.lat} lng={center.lng} zoom={zoom}/>
       </div>
     );
   }
-}
+
+// export class App extends Component<object, State> {
+//   state = {
+//     lat: -34.397,
+//     lng: 150.644,
+//     zoom: 8
+//   };
+
+//   reposition(city: string) {
+//     switch (city) {
+//       case "tel aviv":
+//         this.setState({ lat: 32.0042938, lng: 34.7615399 });
+//       break;
+//       case "London":
+//         this.setState({ lat: 32.0042938, lng: 34.7615399 });
+//         break;
+//       case "Paris":
+//         this.setState({ lat: 32.0042938, lng: 34.7615399 });
+//         break;
+      
+//       default:
+//         alert("wrong city");
+//     }
+//   }
+
+//   set_zoom = (event: ChangeEvent<HTMLInputElement>) => {
+//     this.setState({ zoom: Number(event.target.value)});
+//   };
+
+//   render() {
+//     log(this.state);
+//     return (
+//       <div className="app">
+//         <TopBar><h1>Google Maps Example in React</h1></TopBar>
+//         <div className="hbox mb20">
+//           <button onClick={() => this.reposition("tel aviv")}>Tel Aviv</button>
+//           <button onClick={() => this.reposition("London")}>London</button>
+//           <button onClick={() => this.reposition("Paris")}>Paris</button>
+//           <input type="number" min="8" max="16" placeholder="8" onChange={this.set_zoom}/>
+//         </div>
+//         <GoogleMap lat={this.state.lat} lng={this.state.lng} zoom={this.state.zoom}/>
+//       </div>
+//     );
+//   }
+// }
--- src/view/GMaps.tsx
+++ src/view/GMaps.tsx
@@ -1,39 +1,63 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 // const log = (...args) => console.log.apply(null, ["GoogleMap -->", ...args]);
 const log = (...args: any[]) => console.log("GoogleMap -->", ...args);
 
 interface Props {
-    lat: number;
-    lng: number;
+  lat: number;
+  lng: number;
+  zoom: number;
 }
 
-export class GoogleMap extends Component<Props> {
+export function GoogleMap(nextProps: Props) {
+  //export class GoogleMap extends Component<Props> {
 
-  mapRef = React.createRef<HTMLDivElement>();
-  theMap: google.maps.Map | null = null;
+  let mapRef = React.createRef<HTMLDivElement>();
+  let theMap: google.maps.Map | null = null;
 
-  shouldComponentUpdate(nextProps: Props) {
+
+  let [center, setCenter] = useState({ lat: nextProps.lat, lng: nextProps.lng });
+  let [zoom, setZoom] = useState(nextProps.zoom);
+  setCenter = () => {
+    (theMap! as google.maps.Map).setCenter({ lat: nextProps.lat, lng: nextProps.lng });
+  }
+  setZoom = () => {
+    (theMap! as google.maps.Map).setZoom(nextProps.zoom);
+  }
+  // mapRef = React.createRef<HTMLDivElement>();
+  // theMap: google.maps.Map | null = null;
+
+  const shouldComponentUpdate = () => {
+    //shouldComponentUpdate(nextProps: Props) {
     log("shouldComponentUpdate >>>>");
     // log("this.props:", this.props);
     // log("this.state:", this.state);
     // log("nextState:", nextState);
     // log("nextProps:", nextProps);
     // log("<<<< shouldComponentUpdate");
-    (this.theMap as google.maps.Map).setCenter({ lat: nextProps.lat, lng: nextProps.lng });
+    setCenter({ lat: center.lat, lng: center.lng });
+    setZoom(zoom);
+    // (this.theMap as google.maps.Map).setCenter({ lat: nextProps.lat, lng: nextProps.lng });
+    // (this.theMap as google.maps.Map).setZoom(nextProps.zoom);
 
     return false;
   }
 
-  componentDidMount() {
+  const componentDidMount = () => {
+    //componentDidMount() {
     // log(this.mapRef);
-    this.theMap = new google.maps.Map(this.mapRef.current as HTMLDivElement, {
-      center: { lat: this.props.lat, lng: this.props.lng },
-      zoom: 8
+    theMap = new google.maps.Map(mapRef.current as HTMLDivElement, {
+      center: { lat: center.lat, lng: center.lng },
+      zoom: zoom
     });
+    // this.theMap = new google.maps.Map(this.mapRef.current as HTMLDivElement, {
+    //   center: { lat: this.props.lat, lng: this.props.lng },
+    //   zoom: 8
+    // });
   }
 
-  render() {
-    return <div ref={this.mapRef} className="map-box" />;
-  }
+  return <div ref={mapRef} className="map-box" />;
+  // render() {
+  //   return <div ref={this.mapRef} className="map-box" />;
+  // }
 
 }
